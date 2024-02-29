6ecfe6c7337a64c0fc138cc0b913d6932d419039
Thing Werks <90361336+thwerks@users.noreply.github.com>
2024-03-01 07:56:50 +0800

    Update Example-TW-Client-telegram.js

--- Example-TW-Client-telegram.js
+++ Example-TW-Client-telegram.js
@@ -1,53 +1,52 @@
 // this an example of telegram remote control, this going inside the tw-client.js file
-user = {        // user configurable block - Telegram and timer function.  
-    telegram: { // enter a case matching your desireable input
-        agent: function (msg) {
-            //  log("incoming telegram message: " + msg, 0, 0);
-            //  console.log("incoming telegram message: ", msg);
-            if (sys.telegram.auth(msg)) {
-                switch (msg.text) {
-                    case "?": onsole.log("test help menu"); break;
-                    case "/start": bot(msg.chat.id, "you are already registered");; break;
-                    case "R":   // to include uppercase letter in case match, we put no break between upper and lower cases
-                    case "r":
-                        bot(msg.from.id, "Test Menu:");
-                        setTimeout(() => {      // delay to ensure menu Title gets presented first in Bot channel
-                            sys.telegram.buttonToggle(msg, "t1", "Test Button");
+    user = {        // user configurable block - Telegram 
+        telegram: { // enter a case matching your desireable input
+            agent: function (msg) {
+                //  log("incoming telegram message: " + msg, 0, 0);
+                //  console.log("incoming telegram message: ", msg);
+                if (sys.telegram.auth(msg)) {
+                    switch (msg.text) {
+                        case "?": console.log("test help menu"); break;
+                        case "/start": bot(msg.chat.id, "you are already registered"); break;
+                        case "R":   // to include uppercase letter in case match, we put no break between upper and lower cases
+                        case "r":
+                            bot(msg.from.id, "Test Menu:");
                             setTimeout(() => {      // delay to ensure menu Title gets presented first in Bot channel
-                                sys.telegram.buttonMulti(msg, "t2", "Test Choices", ["test1", "test2", "test3"]);
+                                sys.telegram.buttonToggle(msg, "t1", "Test Button");
+                                setTimeout(() => {      // delay to ensure menu Title gets presented first in Bot channel
+                                    sys.telegram.buttonMulti(msg, "t2", "Test Choices", ["test1", "test2", "test3"]);
+                                }, 200);
                             }, 200);
-                        }, 200);
-                        break;
-                    default:
-                        log("incoming telegram message - unknown command - " + JSON.stringify(msg.text), 0, 0);
-                        break;
-                }
-            }
-            else if (msg.text == cfg.telegram.password) sys.telegram.sub(msg);
-            else if (msg.text == "/start") bot(msg.chat.id, "give me the passcode");
-            else bot(msg.chat.id, "i don't know you, go away");
-
-        },
-        response: function (msg) {  // enter a two character code to identify your callback "case" 
-            let code = msg.data.slice(0, 2);
-            let data = msg.data.slice(2);
-            switch (code) {
-                case "t1":  // read button input and toggle corresponding function
-                    if (data == "true") { myFunction(true); break; }
-                    if (data == "false") { myFunction(false); break; }
-                    break;
-                case "t2":  // read button input and perform actions
-                    switch (data) {
-                        case "test1": bot.sendMessage(msg.from.id, log("test1", "Telegram", 0)); break;
-                        case "test2": bot.sendMessage(msg.from.id, log("test2", "Telegram", 0)); break;
-                        case "test3": bot.sendMessage(msg.from.id, log("test3", "Telegram", 0)); break;
+                            break;
+                        default:
+                            log("incoming telegram message - unknown command - " + JSON.stringify(msg.text), 0, 0);
+                            break;
                     }
-                    break;
-            }       // create a function for use with your callback
-            function myFunction(newState) {    // function that reads the callback input and toggles corresponding boolean in Home Assistant
-                bot.sendMessage(msg.from.id, "Test State: " + newState);
-            }
+                }
+                else if (msg.text == cfg.telegram.password) sys.telegram.sub(msg);
+                else if (msg.text == "/start") bot(msg.chat.id, "give me the passcode");
+                else bot(msg.chat.id, "i don't know you, go away");
+
+            },
+            response: function (msg) {  // enter a two character code to identify your callback "case" 
+                let code = msg.data.slice(0, 2);
+                let data = msg.data.slice(2);
+                switch (code) {
+                    case "t1":  // read button input and toggle corresponding function
+                        if (data == "true") { myFunction(true); break; }
+                        if (data == "false") { myFunction(false); break; }
+                        break;
+                    case "t2":  // read button input and perform actions
+                        switch (data) {
+                            case "test1": bot.sendMessage(msg.from.id, log("test1", "Telegram", 0)); break;
+                            case "test2": bot.sendMessage(msg.from.id, log("test2", "Telegram", 0)); break;
+                            case "test3": bot.sendMessage(msg.from.id, log("test3", "Telegram", 0)); break;
+                        }
+                        break;
+                }       // create a function for use with your callback
+                function myFunction(newState) {    // function that reads the callback input and toggles corresponding boolean in Home Assistant
+                    bot.sendMessage(msg.from.id, "Test State: " + newState);
+                }
+            },
         },
     },
-},
-
