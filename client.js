e89f483ab9e9003dda1cd3282a5bdbdc106f14de
Thing Werks <90361336+thwerks@users.noreply.github.com>
2024-03-11 09:25:30 +0800

    Update client.js

--- client.js
+++ client.js
@@ -53,6 +53,57 @@ let
         }
     ];
 let
+    user = {        // user configurable block - Telegram 
+        telegram: { // enter a case matching your desireable input
+            agent: function (msg) {
+                //  log("incoming telegram message: " + msg, 0, 0);
+                //  console.log("incoming telegram message: ", msg);
+                if (telegram.auth(msg)) {
+                    switch (msg.text) {
+                        case "?": console.log("test help menu"); break;
+                        case "/start": bot(msg.chat.id, "you are already registered"); break;
+                        case "R":   // to include uppercase letter in case match, we put no break between upper and lower cases
+                        case "r":
+                            bot(msg.from.id, "Test Menu:");
+                            setTimeout(() => {      // delay to ensure menu Title gets presented first in Bot channel
+                                telegram.buttonToggle(msg, "t1", "Test Button");
+                                setTimeout(() => {      // delay to ensure menu Title gets presented first in Bot channel
+                                    telegram.buttonMulti(msg, "t2", "Test Choices", ["test1", "test2", "test3"]);
+                                }, 200);
+                            }, 200);
+                            break;
+                        default:
+                            log("incoming telegram message - unknown command - " + JSON.stringify(msg.text), 0, 0);
+                            break;
+                    }
+                }
+                else if (msg.text == cfg.telegram.password) telegram.sub(msg);
+                else if (msg.text == "/start") bot(msg.chat.id, "give me the passcode");
+                else bot(msg.chat.id, "i don't know you, go away");
+
+            },
+            response: function (msg) {  // enter a two character code to identify your callback "case" 
+                let code = msg.data.slice(0, 2);
+                let data = msg.data.slice(2);
+                switch (code) {
+                    case "t1":  // read button input and toggle corresponding function
+                        if (data == "true") { myFunction(true); break; }
+                        if (data == "false") { myFunction(false); break; }
+                        break;
+                    case "t2":  // read button input and perform actions
+                        switch (data) {
+                            case "test1": bot.sendMessage(msg.from.id, log("test1", "Telegram", 0)); break;
+                            case "test2": bot.sendMessage(msg.from.id, log("test2", "Telegram", 0)); break;
+                            case "test3": bot.sendMessage(msg.from.id, log("test3", "Telegram", 0)); break;
+                        }
+                        break;
+                }       // create a function for use with your callback
+                function myFunction(newState) {    // function that reads the callback input and toggles corresponding boolean in Home Assistant
+                    bot.sendMessage(msg.from.id, "Test State: " + newState);
+                }
+            },
+        },
+    },
     sys = {
         com: function () {
             udp.on('message', function (data, info) {
@@ -73,7 +124,7 @@ let
                         case "haStateUpdate":       // incoming state change (from HA websocket service)
                             log("receiving state data, entity: " + cfg.ha[buf.obj.id] + " value: " + buf.obj.state, 0);
                             //         console.log(buf);
-                            state.ha[buf.obj.id] = buf.obj.state;
+                            try { state.ha[buf.obj.id] = buf.obj.state; } catch { }
                             if (state.online == true) {
                                 em.emit(cfg.ha[buf.obj.id], buf.obj.state);
                                 automation.forEach((func, index) => { if (state.auto[index]) func(index) });
@@ -171,12 +222,13 @@ let
                     "[Install]",
                     "WantedBy=multi-user.target\n",
                     "[Service]",
-                    "ExecStart=nodemon " + cfg.workingDir + "client-" + moduleName + ".js -w " + cfg.workingDir + "client-" + moduleName + ".js",
+                    "ExecStart=nodemon " + cfg.workingDir + "client-" + moduleName + ".js -w " + cfg.workingDir + "client-" + moduleName + ".js --exitcrash",
                     "Type=simple",
                     "User=root",
                     "Group=root",
                     "WorkingDirectory=" + cfg.workingDir,
-                    "Restart=on-failure\n",
+                    "Restart=on-failure",
+                    "RestartSec=10\n",
                 ];
                 fs.writeFileSync("/etc/systemd/system/tw-client-" + moduleName + ".service", service.join("\n"));
                 // execSync("mkdir /apps/ha -p");
